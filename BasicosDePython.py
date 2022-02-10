9704fdcd89beafe20b107cbb754fc2bdb4b62f39
Jose angel <73683594+josearg150@users.noreply.github.com>
2022-02-10 11:54:40 -0600

    sintaxis basica

---
+++ BasicosDePython.py
@@ -0,0 +1,28 @@
+#Ddentacion
+if 3 > 5:
+    print('Esto no se va a imprimir')
+#Los comentarios se agregan utilizando el numeral
+#Comentario
+
+#Variables
+x = 5
+y = 'Palabra'
+
+print(x, y)
+
+email = 'ejemplo@gmail.com'
+
+print(email)
+
+#multiples variables en una linea
+a, b, c = 'uno', ' dos', 'tres'
+print(a, b, c)
+
+valor1 = valor2 = valor3 = "valores"
+print(valor1, valor2, valor3)
+
+#Concatenacion
+inicio = 'Hola '
+final = 'Mundo'
+
+print(inicio + final)
